768b04ef8585a75bb8bb387ffd7b9af9173a2fc8
Piotr Wronarowicz <gawrone@interia.pl>
2018-03-12 23:11:40 +0100

    Cwiczenie2

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,19 @@
 // scripts.js
-$(function(){
-  var paragraphs = $('p').css('color', 'red');
-  var rawFirstParagraph = paragraphs[0];
-  console.log(rawFirstParagraph);
-  var jqFirstParagraph = paragraphs.eq(0)
-  console.log(jqFirstParagraph);
-  jqFirstParagraph.css('color', 'blue');
+var span = $("span");
+    span.each(function(index, element) {
 });
+span.each(function(index, element) {
+	if(index % 2 == 0) {
+		$(element).css('color', 'red');
+	};
+});
+var paragraphs = $('p');
+paragraphs.each(function(index, element) {
 
-	$(function(){
-  
-		$("ul").append("<li>New element</li>")
-		
-		var firstItem = $('nav').find('li').first();
-	 
-		console.log(firstItem.text());
-		
-		firstItem.css("color", "red")
-	 
-	 
-	});
+    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+    $(element).append(button)
+
+});
+$("button").click(function(){
+	alert($(this).attr("data-tmp"));
+});
