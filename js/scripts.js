b2bccc63e4f258d4d40382afeb332fc859643543
Piotr Wronarowicz <gawrone@interia.pl>
2018-04-09 18:18:05 +0200

    Attempt at left right buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
 // scripts.js
 $(function(){
-  var carouselList = $('#carousel ul');
-  setInterval(changeSlide, 3000);
-  carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-  var firstItem = carouselList.find('li:first');
-  var lastItem = carouselList.find('li:last');
+	var carouselList = $("#carousel ul");
+	var interval = setInterval(changeSlide, 3000);
+	function changeSlide() {
+    carouselList.animate({'marginLeft': -400}, 500, moveFirstSlide);
+	}
+	function moveFirstSlide() {
+  		var firstItem = carouselList.find("li:first");
+  		var lastItem = carouselList.find("li:last");
+    	lastItem.after(firstItem);
+    	carouselList.css({marginLeft:0});
+}
 });
-  
+$(".control_next").click(function () {
+  $(this).show("#carousel ul", { direction: "right" }, 1000)});
+$(".control_prev").click(function () {
+  $(this).show("#carousel ul", { direction: "left" }, 1000)});
